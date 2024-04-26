ee77232972c8cf0fae87fca51a1f9d85d035b86c
bekrenov-r <bekrenov.roman@gmail.com>
2024-04-26 15:42:51 +0200

    Init frontend and backend projects

---
+++ backend/src/test/java/com/bekrenovr/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bekrenovr.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
