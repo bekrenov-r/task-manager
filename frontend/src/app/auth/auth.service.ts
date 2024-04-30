42612d369b12ec95fdc6a1f56a59bcfcc757c932
bekrenov-r <bekrenov.roman@gmail.com>
2024-04-30 17:08:54 +0200

    Add logout functionality, add full name of current user to header

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -1,15 +1,22 @@
 import { HttpClient, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, of, tap } from 'rxjs';
 import { environment } from '../../environments/environment';
 import { Registration } from '../models/registration';
+import jwt_decode, { jwtDecode } from 'jwt-decode';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  jwtPayload: any;
+  private _isAuthenticated$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor(private http: HttpClient) { }
+
+  get isAuthenticated$(): Observable<boolean> {
+    return this._isAuthenticated$.asObservable();
+  }
 
   login(username: string, password: string): Observable<any> {
     return this.http.get(environment.apiBaseUrl + "/users/authenticate", {
@@ -19,8 +26,10 @@ export class AuthService {
       },
       responseType: 'text'
     }).pipe(
-      tap(value => {
-        localStorage.setItem(environment.authTokenLocalStorageKey, value);
+      tap(token => {
+        localStorage.setItem(environment.authTokenLocalStorageKey, token);
+        this._isAuthenticated$.next(true);
+        this.jwtPayload = jwtDecode(token);
       })
     );
   }
@@ -34,6 +43,17 @@ export class AuthService {
 
   isAuthenticated(): boolean {
     return Object.keys(localStorage).some(key => key === environment.authTokenLocalStorageKey);
+  }
+
+  logout(): void {
+    localStorage.removeItem(environment.authTokenLocalStorageKey);
+  }
+
+  getJwtPayload(): any {
+    console.log(jwtDecode(localStorage.getItem(environment.authTokenLocalStorageKey)!));
+    console.log(localStorage.getItem(environment.authTokenLocalStorageKey)!);
+    
+    return jwtDecode(localStorage.getItem(environment.authTokenLocalStorageKey)!);
   }
 }
 
--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,13 +1,41 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { JwtPayload } from 'jwt-decode';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
   encapsulation: ViewEncapsulation.None
 })
 export class NavbarComponent {
+  fullName: string = '';
+  isAuthenticated: boolean = false;
+  jwtPayload$: Observable<any>;
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated();
+    this.authService.isAuthenticated$.subscribe(value => { 
+      this.isAuthenticated = value;
+      if(this.isAuthenticated){
+        this.fullName = this.authService.getJwtPayload()['fullName'];
+      }
+    });
+    this.isAuthenticated = this.authService.isAuthenticated();
+    if(this.isAuthenticated){
+      this.fullName = this.authService.getJwtPayload()['fullName'];
+    }
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
